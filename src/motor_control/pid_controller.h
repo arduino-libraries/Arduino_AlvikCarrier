e31cc31cf33a9612e8eee874958ee0fba2f992e4
Giovanni Bruno <g.bruno@arduino.cc>
2024-01-17 15:53:02 +0100

    pid cleanup

--- src/motor_control/pid_controller.h
+++ src/motor_control/pid_controller.h
@@ -38,89 +38,23 @@ class PidController{
 
 
     public:
-        PidController(const float _kp, const float _ki, const float _kd, const float _ctrl_period, const int _ctrl_limit = 4095){
-            kp=_kp;
-            ki=_ki;
-            kd=_kd;
-            ctrl_period=_ctrl_period;
-            ctrl_limit=_ctrl_limit;
-            
-            reference=0.0;
-            error=0.0;
-            error_rate=0.0;
-            previous_error=0.0;
-            error_sum=0.0;
-            last_measure=0.0;
+        PidController(const float _kp, const float _ki, const float _kd, const float _ctrl_period, const int _ctrl_limit = 4095);
 
-            ctrl_p=0.0;
-            ctrl_i=0.0;
-            ctrl_d=0.0;
-            ctrl_output=0.0;
-        }
+        void setReference(const float _reference);
 
-        void setReference(const float _reference){
-            reference=_reference;
-        }
+        float getControlOutput();
+        float getError();
 
-        float getControlOutput(){
-            return ctrl_output;
-        }
+        void setKPid(const float _kp, const float _ki, const float _kd);
+        void setKp(const float _k);
+        void setKi(const float _k);
+        void setKd(const float _k);
 
-        float getError(){
-            return error;
-        }
+        void update(const float measure);
 
-        void setKPid(const float _kp, const float _ki, const float _kd){
-            kp=_kp;
-            ki=_ki;
-            kd=_kd;
-        }
+        float checkLimits(float value);
 
-        void setKp(const float _k){
-            kp=_k;
-        }
-
-        void setKi(const float _k){
-            ki=_k;
-        }
-
-        void setKd(const float _k){
-            kd=_k;
-        }
-
-        void update(const float measure){
-            error = reference - measure;
-            error_sum += error*ctrl_period;
-            error_rate = (measure-last_measure)/ctrl_period;
-            last_measure=measure;
-            ctrl_p = error*kp;
-            ctrl_i = checkLimits(error_sum*ki);
-            ctrl_d = error_rate*kd;
-            ctrl_output = checkLimits(ctrl_p+ctrl_i-ctrl_d);
-        }
-
-        float checkLimits(float value){
-            if (value>ctrl_limit){
-                return ctrl_limit;
-            }
-            if (value<-ctrl_limit){
-                return -ctrl_limit;
-            }
-            return value;
-        }
-
-        void reset(){
-            error=0.0;
-            error_sum=0.0;
-            previous_error=0.0;
-            error_rate=0.0;
-            reference=0.0;
-            last_measure=0.0;
-            ctrl_p=0.0;
-            ctrl_i=0.0;
-            ctrl_d=0.0;
-            ctrl_output=0.0;
-        }
+        void reset();
 };
 
 
