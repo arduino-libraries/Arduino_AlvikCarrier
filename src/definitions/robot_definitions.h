a063a19d88fc52ae636e7e32f0fd2e218caeb737
Lucio Rossi <lucio.rossi75@gmail.com>
2025-03-19 17:03:51 +0100

    ver. 1.1.1 ready

--- src/definitions/robot_definitions.h
+++ src/definitions/robot_definitions.h
@@ -93,7 +93,7 @@ const float MOTION_FX_PERIOD = (1000U / MOTION_FX_FREQ);
 // Library version
 #define VERSION_BYTE_HIGH 1
 #define VERSION_BYTE_MID 1
-#define VERSION_BYTE_LOW 0
+#define VERSION_BYTE_LOW 1
 
 // Battery stats
 #define BATTERY_ALERT_MINIMUM_CHARGE 20.0
