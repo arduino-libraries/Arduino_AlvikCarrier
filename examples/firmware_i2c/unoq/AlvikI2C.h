f8dbd06aad4e84616b4107687a9e58a647934058
Lucio Rossi <lucio.rossi75@gmail.com>
2026-01-07 14:59:17 +0100

    unoq: AlvikI2C class

---
+++ examples/firmware_i2c/unoq/AlvikI2C.h
@@ -0,0 +1,52 @@
+
+#ifndef ARDUINO_ALVIKCARRIER_ALVIKI2C_H
+#define ARDUINO_ALVIKCARRIER_ALVIKI2C_H
+
+#include <Wire.h>
+#include <ucPack.h>
+#define wire Wire1
+
+#define ALVIK_I2C_ADDRESS 0x2B
+
+class AlvikI2C{
+
+    int address{};
+    uint8_t version[3] = {0xFF, 0xFF, 0xFF};
+
+    public:
+    AlvikI2C(int addr=ALVIK_I2C_ADDRESS){
+        address = addr;
+    }
+
+    void begin() {
+        wire.begin();
+        getVersion();
+    }
+
+    bool isOnline() {
+        wire.beginTransmission(address);
+        uint8_t error = wire.endTransmission();
+        return (error == 0);
+    }
+
+    void getVersion() {
+        wire.beginTransmission(address);
+        wire.write('V');
+        wire.endTransmission();
+        wire.requestFrom((uint8_t)address, (uint8_t)3);
+        wire.readBytes(version, 3);
+    }
+
+    String getVersionString() {
+        String versionStr = String(version[0]) + "." +
+                            String(version[1]) + "." +
+                            String(version[2]);
+        return versionStr;
+    }
+
+
+};
+
+
+
+#endif //ARDUINO_ALVIKCARRIER_ALVIKI2C_H
--- examples/firmware_i2c/unoq/unoq.ino
+++ examples/firmware_i2c/unoq/unoq.ino
@@ -1,24 +1,21 @@
-#include "Wire.h"
 #include "Arduino_RouterBridge.h"
-#include "ucPack.h"
-#define wire Wire1
+#include "AlvikI2C.h"
 
-#define ALVIK_I2C_ADDRESS 0x2B
 
-ucPack packeter(200);
-uint8_t version[3];
-int address = ALVIK_I2C_ADDRESS;
+AlvikI2C alvik;
+
 int msg_size;
 
 float f = 90;
 uint8_t msg[100];
 
 void setup() {
-    wire.begin();
+
+    alvik.begin();
     Bridge.begin();
     Monitor.begin();
 
-    while (!scanForAlvik()) {
+    while (!alvik.isOnline()) {
         Monitor.println("Alvik not found");
         delay(1000);
     }
@@ -40,51 +37,36 @@ void print_msg(size_t size) {
 
 void loop() {
 
-    wire.beginTransmission(address);
-    wire.write('V');
-    wire.endTransmission();
-    wire.requestFrom((uint8_t)address,(uint8_t)3);
-    wire.readBytes(version,3);
-
-    Monitor.print(version[0], HEX);
-    Monitor.print("\t");
-    Monitor.print(version[1], HEX);
-    Monitor.print("\t");
-    Monitor.println(version[2], HEX);
+    Monitor.println(alvik.getVersionString());
     delay(1000);
 
-    f = 90.0;
-    wire.beginTransmission(address);
-    wire.write('R');
-    //msg_size = packeter.packetC1F('R', f);
-    memcpy(msg, &f, sizeof(f));
-    //wire.write(packeter.msg, msg_size);
-    wire.write(msg, sizeof(f));
-    wire.endTransmission();
-    delay(1000);
-
-    f = -90.0;
-    wire.beginTransmission(address);
-    wire.write('R');
-    //msg_size = packeter.packetC1F('R', f);
-    memcpy(msg, &f, sizeof(f));
-    //wire.write(packeter.msg, msg_size);
-    wire.write(msg, sizeof(f));
-    wire.endTransmission();
-    delay(1000);
-
-    wire.beginTransmission(address);
-    wire.write('I');
-    wire.endTransmission();
-    //delay(20);
-    wire.requestFrom((uint8_t)address,(uint8_t)29);
-    wire.readBytes(msg,29);
-    print_msg(29);
-    delay(1000);
+//    f = 90.0;
+//    wire.beginTransmission(address);
+//    wire.write('R');
+//    //msg_size = packeter.packetC1F('R', f);
+//    memcpy(msg, &f, sizeof(f));
+//    //wire.write(packeter.msg, msg_size);
+//    wire.write(msg, sizeof(f));
+//    wire.endTransmission();
+//    delay(1000);
+//
+//    f = -90.0;
+//    wire.beginTransmission(address);
+//    wire.write('R');
+//    //msg_size = packeter.packetC1F('R', f);
+//    memcpy(msg, &f, sizeof(f));
+//    //wire.write(packeter.msg, msg_size);
+//    wire.write(msg, sizeof(f));
+//    wire.endTransmission();
+//    delay(1000);
+//
+//    wire.beginTransmission(address);
+//    wire.write('I');
+//    wire.endTransmission();
+//    //delay(20);
+//    wire.requestFrom((uint8_t)address,(uint8_t)29);
+//    wire.readBytes(msg,29);
+//    print_msg(29);
+//    delay(1000);
 }
 
-bool scanForAlvik() {
-    wire.beginTransmission(address);
-    uint8_t error = wire.endTransmission();
-    return (error == 0);
-}
